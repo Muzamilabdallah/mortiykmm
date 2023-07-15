fb93dc4a1a3e7292e898292bae30db6cab1b84b5
muzamil <47520464+Muzamilabdallah@users.noreply.github.com>
2023-07-15 18:44:24 +0300

    Initial commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("com.apollographql.apollo3")
+}
+
+kotlin {
+    androidTarget()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.coroutines.core)
+                implementation(libs.koin.core)
+
+                api(libs.apollo.runtime)
+                implementation(libs.apollo.normalized.cache)
+                implementation(libs.apollo.normalized.cache.sqlite)
+
+                api(libs.multiplatformPaging)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+            }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+
+    }
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    namespace = "dev.johnoreilly.mortycomposekmm.shared"
+}
+
+
+apollo {
+    packageName.set("dev.johnoreilly.mortycomposekmm")
+    generateOptionalOperationVariables.set(false)
+}
